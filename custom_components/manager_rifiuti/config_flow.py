c04f8e16cc692354d0d6735e00c2dca02cb20bc7
Evuse <carloebasta@gmail.com>
2026-08-11 01:19:32 +0200

    Add customizable notification testing in custom_components/manager_rifiuti/config_flow.py

--- custom_components/manager_rifiuti/config_flow.py
+++ custom_components/manager_rifiuti/config_flow.py
@@ -8,11 +8,21 @@ from homeassistant.const import CONF_WEBHOOK_ID
 from homeassistant.core import callback
 
 from .const import (
+    CONF_ACTION_TITLE,
     CONF_ENABLED_WASTE,
+    CONF_NOTIFY_MESSAGE,
     CONF_NOTIFY_SERVICES,
     CONF_NOTIFY_TIME,
+    CONF_NOTIFY_TITLE,
+    CONF_REMINDER_MESSAGE,
     CONF_REMINDER_TIME,
+    CONF_REMINDER_TITLE,
+    DEFAULT_ACTION_TITLE,
+    DEFAULT_NOTIFY_MESSAGE,
     DEFAULT_NOTIFY_TIME,
+    DEFAULT_NOTIFY_TITLE,
+    DEFAULT_REMINDER_MESSAGE,
+    DEFAULT_REMINDER_TITLE,
     DOMAIN,
     WASTE_NAMES,
 )
@@ -59,6 +69,26 @@ class OptionsFlow(config_entries.OptionsFlow):
                 vol.Optional(
                     CONF_ENABLED_WASTE,
                     default=current.get(CONF_ENABLED_WASTE, ",".join(WASTE_NAMES)),
+                ): str,
+                vol.Optional(
+                    CONF_NOTIFY_TITLE,
+                    default=current.get(CONF_NOTIFY_TITLE, DEFAULT_NOTIFY_TITLE),
+                ): str,
+                vol.Optional(
+                    CONF_NOTIFY_MESSAGE,
+                    default=current.get(CONF_NOTIFY_MESSAGE, DEFAULT_NOTIFY_MESSAGE),
+                ): str,
+                vol.Optional(
+                    CONF_REMINDER_TITLE,
+                    default=current.get(CONF_REMINDER_TITLE, DEFAULT_REMINDER_TITLE),
+                ): str,
+                vol.Optional(
+                    CONF_REMINDER_MESSAGE,
+                    default=current.get(CONF_REMINDER_MESSAGE, DEFAULT_REMINDER_MESSAGE),
+                ): str,
+                vol.Optional(
+                    CONF_ACTION_TITLE,
+                    default=current.get(CONF_ACTION_TITLE, DEFAULT_ACTION_TITLE),
                 ): str,
             }
         )
